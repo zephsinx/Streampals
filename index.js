cc1193ad995cdb6fbbfea53fef27ed056c3451ac
zephsinx <zephsinx@gmail.com>
2022-12-23 11:22:37 -0500

    And .env file and rename public to src

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-// todo: Create and use environment variables
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
 // Defining port number
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/src'));
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '/public/html/index.html'));
+    res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 // Server setup
 app.listen(PORT, () => {
     console.log(`Running server on PORT ${PORT}...`);
-})
+});
