1462c0dce7a49648c096196475e924b413e5ebdf
zephsinx <117953858+zephsinx@users.noreply.github.com>
2022-12-24 10:23:12 -0500

    Randomize corners and add max height and width params (#1)
    * Add favicon and rework param validations
    
    * Prevent duplicate corners and accept size params

--- src/streamerworm.js
+++ src/streamerworm.js
@@ -4,23 +4,26 @@
 const constants = require('./constants');
 const media = require('./medialist');
 
-// Set default time bounds to use if none are provided
+// Global defaults
 const defaultMinMillis = constants.DefaultMinMinutes * 60 * 1000;
 const defaultMaxMillis = constants.DefaultMaxMinutes * 60 * 1000;
+
+// Global values
+let lastCorner = randomIntFromInterval(0, 3);
+
+// Get config settings
+const config = getStreamerWormConfig();
 
 // Set default media to the first entry in the list
 const mediaListElement = media.MediaList[0];
 
 // Build media element based on file extension
 const tagName = getTagNameFromFile(mediaListElement.path);
-const element = prepareElement(tagName);
+const element = prepareElement(tagName, config);
 
 // Append media element to media div
 const mediaDiv = document.getElementById("media-div");
 mediaDiv.appendChild(element);
-
-// Get config settings
-const config = getStreamerWormConfig();
 
 // Initialize media loop
 playMedia(element);
@@ -30,9 +33,7 @@ playMedia(element);
 // Shows and plays media after a random delay, then hides the media after durationMillis expires.
 function playMedia(element) {
     // Skip delay between media plays when config.skipDelay == true
-    let delay = config.skipDelay
-        ? 0
-        : randomIntFromInterval(config.minDelay, config.maxDelay);
+    let delay = randomIntFromInterval(config.minDelay, config.maxDelay);
     
     // Display the image after the random delay expires
     setTimeout(() => {
@@ -51,8 +52,8 @@ function playMedia(element) {
         
         // Hide image/video after it plays for the desired duration, and requeue the media timer
         setTimeout(() => {
-
             element.style.visibility = 'hidden';
+            setPosition(element);
             playMedia(element);
         }, mediaListElement.durationMillis);
         
@@ -62,6 +63,42 @@ function playMedia(element) {
 // Returns a random integer between min and max (inclusive)
 function randomIntFromInterval(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function setPosition(element) {
+    let corner = randomIntFromInterval(0, 3);
+    while (lastCorner === corner) {
+        corner = randomIntFromInterval(0, 3);
+    }
+    lastCorner = corner;
+
+    element.style.top = '';
+    element.style.bottom = '';
+    element.style.left = '';
+    element.style.right = '';
+
+    switch (corner) {
+        // 0: top left
+        case 0:
+            element.style.top = '0px';
+            element.style.left = '0px';
+            break;
+        // 1: top right
+        case 1:
+            element.style.top = '0px';
+            element.style.right = '0px';
+            break;
+        // 2: bottom right
+        case 2:
+            element.style.bottom = '0px';
+            element.style.right = '0px';
+            break;
+        // 3: bottom left
+        case 3:
+            element.style.bottom = '0px';
+            element.style.left = '0px';
+            break;
+    }
 }
 
 //#endregion
@@ -77,29 +114,47 @@ function getStreamerWormConfig() {
 
     // todo: (param) Image display coordinates (where on the screen should it show up)
     let skipDelay = parseBool(urlParams.skipDelay);
-    let minDelayMillis = isValidDelay(urlParams.min) ? (urlParams.min * 60 * 1000) : defaultMinMillis; 
-    let maxDelayMillis = isValidDelay(urlParams.max) ? (urlParams.max * 60 * 1000) : defaultMaxMillis;
-    let slideshow = parseBool(urlParams.slideshow);
-    let shouldRandomize = parseBool(urlParams.randomize);
+    let maxDelayMillis = getDelayMillis(skipDelay, urlParams.max, defaultMaxMillis);
+    let minDelayMillis = getDelayMillis(skipDelay, urlParams.min, defaultMinMillis);
+    let maxHeight = isValidNumericValue(urlParams.maxHeight) ? urlParams.maxHeight : constants.DefaultMaxHeight;
+    let maxWidth = isValidNumericValue(urlParams.maxWidth) ? urlParams.maxWidth : constants.DefaultMaxWidth;
+    // let shouldRandomize = parseBool(urlParams.randomize);
+    // let slideshow = parseBool(urlParams.slideshow);
+    
+    let config = {
+        maxDelay: maxDelayMillis,         // The maximum delay between media plays (ignored if skipDelay is true)
+        minDelay: minDelayMillis,         // The minimum delay between media plays (ignored if skipDelay is true)
+        maxHeight: maxHeight,             // The maximum height the media should take up. Image will be resized to fit if larger.
+        maxWidth: maxWidth,               // The maximum width the media should take up. Image will be resized to fit if larger.
+        // shouldRandomize: shouldRandomize, // If the displayed media should be randomized from the media list (ignored if slideshow is false)
+        // slideshow: slideshow,             // If the displayed media should change on each loop
+    };
+    
+    return validateConfig(config);
+}
 
-    // minDelayMillis must be less than maxDelayMillis
-    if (maxDelayMillis <= minDelayMillis)
+// Validate and update config if invalid.
+function validateConfig(config) {
+    // minDelayMillis must be less than or equal to maxDelayMillis, else use defaults
+    if (config.maxDelayMillis < config.minDelayMillis)
     {
-        minDelayMillis = defaultMinMillis;
-        maxDelayMillis = defaultMaxMillis;
+        config.maxDelayMillis = defaultMaxMillis;
+        config.minDelayMillis = defaultMinMillis;
     }
     
-    return {
-        skipDelay: skipDelay,             // If the delay between media plays should be skipped
-        minDelay: minDelayMillis,         // The minimum delay between media plays (ignored if skipDelay is true)
-        maxDelay: maxDelayMillis,         // The maximum delay between media plays (ignored if skipDelay is true)
-        slideshow: slideshow,             // If the displayed media should change on each loop
-        shouldRandomize: shouldRandomize, // If the displayed media should be randomized from the media list (ignored if slideshow is false)
-    };
+    return config;
+}
+
+// Validate and calculate delay in milliseconds from params
+function getDelayMillis(skipDelay, delayMinutes, defaultDelay) {
+    if (skipDelay)
+        return 0;
+
+    return isValidNumericValue(delayMinutes) ? (delayMinutes * 60 * 1000) : defaultDelay;
 }
 
 // Check that provided string is a valid number and positive.
-function isValidDelay(numberString) {
+function isValidNumericValue(numberString) {
     return !isNaN(numberString) && !isNaN(parseFloat(numberString)) && parseFloat(numberString) > 0;
 }
 
@@ -113,11 +168,14 @@ function parseBool(boolString) {
 //#region Element Configuration
 
 // Configure element to display based on tagName
-function prepareElement(tagName) {
+function prepareElement(tagName, config) {
     // Create img or video element based on tagName
     let mediaElement = document.createElement(tagName);
     mediaElement.id = 'rendered-media';
-    mediaElement.style = 'max-width: 25%; max-height: 25%; object-fit: contain';
+    mediaElement.style.objectFit = 'contain';
+    mediaElement.style.maxHeight = config.maxHeight + '%';
+    mediaElement.style.maxWidth = config.maxWidth + '%';
+    mediaElement.style.position = 'absolute';
     
     switch (tagName) {
         case 'img':
@@ -125,7 +183,7 @@ function prepareElement(tagName) {
         case 'video':
             return configureVideoElement(mediaElement);
         default:
-            return mediaElement;
+            throw 'Tag name ' + tagName + ' not recognized';
     }
 }
 
