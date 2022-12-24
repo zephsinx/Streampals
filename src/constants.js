bd1397c47d8bbf01d050d233abedeafb9f12b9d9
zephsinx <117953858+zephsinx@users.noreply.github.com>
2022-12-24 10:23:12 -0500

    Randomize corners and add max height and width params (#1)
    * Add favicon and rework param validations
    
    * Prevent duplicate corners and accept size params

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,5 @@
 // StreamerWorm constants.
 exports.DefaultMinMinutes = 30;
 exports.DefaultMaxMinutes = 90;
+exports.DefaultMaxHeight = '25'; // Percentage value
+exports.DefaultMaxWidth = '25'; // Percentage value
