bc0b25b5eba96939d310dd5237f244de0015eca5
zephsinx <117953858+zephsinx@users.noreply.github.com>
2022-12-24 12:21:13 -0500

    Add webpack cache busting for JS files (#2)
    * Add cache busting and remove some media files
    
    * Fix media playing twice in upper left corner
    
    * Update CHANGELOG

--- src/medialist.js
+++ src/medialist.js
@@ -1,7 +1,5 @@
 // Array of image/video paths and their display duration.
-// Note: Image names must match values in /dist/images
+// Note: Image names must match values in /dist/media
 exports.MediaList = [
     { path: 'media/worms-transparent.gif', durationMillis: 4410 },
-    { path: 'media/big-buck-bunny_trailer.webm', durationMillis: 31000 },
-    { path: 'media/worms.gif', durationMillis: 3290 },
 ];
